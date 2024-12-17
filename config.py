ea9500ab625db10ee90a2f4fcf17bcba3a283a36
Skiba <87806241+Skiba95@users.noreply.github.com>
2024-12-17 15:54:12 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,48 @@
+
+#--------------------------------------- main-config ---------------------------------------#
+
+cex = "binance"           # binance, mexc, kucoin, gate, okx, huobi, bybit, bitget, coinex
+symbolWithdraw = "BNB"  
+network = "BSC"        
+amount = [0.0012, 0.0014]        # min & max amount
+decimal_places = 6       # the number of decimal places for generating random numbers
+delay = [30, 68]       # min & max delay
+shuffle_wallets = "no"   # yes/no
+proxy_server = "http://login:password@IP:port"
+
+#---------------------------------- telegram-notification ----------------------------------#
+
+chat = ""
+bot_id = ""
+
+#----------------------------------------- api-keys ----------------------------------------#
+
+binance_apikey = ""
+binance_apisecret = ""
+
+okx_apikey = ""
+okx_apisecret = ""
+okx_passphrase = ""
+
+bybit_apikey = "your_api"
+bybit_apisecret = "your_api_secret"
+
+gate_apikey = ""
+gate_apisecret = ""
+
+kucoin_apikey = "your_api"
+kucoin_apisecret = "your_api_secret"
+kucoin_passphrase = "your_api_password"
+
+mexc_apikey = "your_api"
+mexc_apisecret = "your_api_secret"
+
+huobi_apikey = "your_api"
+huobi_apisecret = "your_api_secret"
+
+bitget_apikey = ""
+bitget_apisecret = ""
+bitget_password = ""
+
+coinex_apikey = ""
+coinex_apisecret = ""
